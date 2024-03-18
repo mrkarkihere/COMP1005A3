63a0b3507419746e023d2893303f9a499652c0c3
Arun Karki <arunkarki3629@gmail.com>
2024-03-18 19:38:38 -0400

    updates

--- DatabaseApplication.java
+++ DatabaseApplication.java
@@ -1,6 +1,11 @@
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+
 
 public class DatabaseApplication{
 
@@ -12,6 +17,43 @@ public class DatabaseApplication{
         this.user = user;
         this.password = password;
         this.connect();
+    }
+
+    public void getAllStudents() throws SQLException {
+        String query = "SELECT * FROM students";
+        Statement stmt = conn.createStatement();
+        ResultSet result = stmt.executeQuery(query);
+
+        while(result.next()){
+            System.out.println(
+                result.getInt("student_id") + "\t" +
+                result.getString("first_name") + "\t" +
+                result.getString("last_name") + "\t" +
+                result.getString("email") + "\t" +
+                result.getDate("enrollment_date")
+            );
+        }
+        result.close();
+        stmt.close();
+    }
+
+    public void addStudent(String firstName, String lastName, String email, String enrollmentDate) throws SQLException{
+
+        String newStudent = "('" + firstName + "', '" + lastName + "', '" + email + "', '" + enrollmentDate + "');";
+        String query = ("INSERT INTO students (first_name, last_name, email, enrollment_date) VALUES " + newStudent);
+
+        Statement stmt = conn.createStatement();
+        int result = stmt.executeUpdate(query);
+
+        if(result > 1){
+            System.out.println("A student was inserted successfully!");
+        }
+        stmt.close();
+    }
+
+    public void updateStudentEmail(int student_id, String new_email){
+        String query = "UPDATE students SET email = " + new_email + " WHERE id = " + student_id;
+        System.out.println(query);
     }
 
     public void connect(){
@@ -47,6 +89,14 @@ public class DatabaseApplication{
         String password = "test"; 
     
         DatabaseApplication app = new DatabaseApplication(url, user, password);
+        // testing
+        try {
+            app.getAllStudents();
+            //app.addStudent("Arun", "Karki", "ARUNKARKI@cmail.carleton.ca", "2024-03-18");
+            app.updateStudentEmail(1, "hello");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
         app.disconnect();
     }
     
